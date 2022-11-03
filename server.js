2a1f3a57a66fb9eaa3220b37630cf0b32a1427b2
fiamett <108776824+fiamett@users.noreply.github.com>
2022-11-02 22:50:38 -0700

    did a little more forum trolling fixed port issue
    hopefully...

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 
 
 const app = express();
-const PORT = 2130;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
